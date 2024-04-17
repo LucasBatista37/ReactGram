856bebcbf6b9f2e03124af2c94de15dc815fca95
LucasBatista37 <lucas.batista9734@email.com>
2024-04-17 00:47:50 -0300

    backend finalizado

---
+++ backend/middlewares/photoValidation.js
@@ -0,0 +1,45 @@
+const {body} = require("express-validator")
+
+const photoInsertValidation = () => {
+    return [
+        body("title")
+        .not()
+        .equals("undefined")
+        .withMessage("O título é obrigatório")
+        .isString()
+        .withMessage("O titulo é obrigatório.")
+        .isLength({min: 3})
+        .withMessage("O título precisa ter no mínimo 3 caracteres"),
+    body("image").custom((value, {req}) => {
+        if (!req.file) {
+            throw new Error("A imagem é obrigatória.");
+        }
+        return true;
+    })
+    ]
+}
+
+const photoUpdateValidation = () => {
+    return [
+        body("title")
+            .optional()
+            .exists()
+            .withMessage("O título é obrigatório")
+            .isString()
+            .withMessage("O título precisa ser uma string")
+            .isLength({ min: 3 })
+            .withMessage("O título precisa ter no mínimo 3 caracteres")
+    ]
+}
+
+const commentValidation = () => {
+    return [
+        body("comment").isString().withMessage("O comentário é obrigatório")
+    ]
+}
+
+module.exports = {
+    photoInsertValidation,
+    photoUpdateValidation,
+    commentValidation,
+}
